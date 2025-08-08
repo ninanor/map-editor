458ba922bf48da39d1c55aa141489cb675580da2
Niccolo Cantu <niccolo.cantu@nina.no>
2025-08-08 12:53:29 +0200

    fix: edit overflow

--- src/routes/editor/_layout/edit.tsx
+++ src/routes/editor/_layout/edit.tsx
@@ -4,6 +4,8 @@ import { EDIT_TABS } from '../../../config';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { useTranslation } from 'react-i18next';
+import { useElementSize } from '@custom-react-hooks/use-element-size';
+import { useMemo } from 'react';
 
 export const Route = createFileRoute('/editor/_layout/edit')({
   component: RouteComponent,
@@ -13,27 +15,40 @@ function RouteComponent() {
   const location = useLocation();
   const { t } = useTranslation();
 
+  const [setRef, size] = useElementSize();
+
+  const outletWrapper = useMemo(
+    () => ({
+      maxHeight: `calc(100vh - var(--spacing) * 4 - ${size.height ?? 0}px)`,
+    }),
+    [size],
+  );
+
   return (
     <div id="edit-bar" className="bg-primary shadow-2xs h-screen w-md p-2 text-primary-content">
-      <h2 className="text-xl font-bold">
-        <FontAwesomeIcon icon={faEdit} className="mr-3" />
-        {t('edit-mode')}
-      </h2>
-      <div role="tablist" className="tabs tabs-border my-3">
-        {EDIT_TABS.map(tab => (
-          <Link
-            role="tab"
-            className={classNames('tab !text-primary-content', {
-              'tab-active !text-accent-content': location.pathname === tab.id,
-            })}
-            to={tab.id}
-            key={tab.id}
-          >
-            {t(tab.label)}
-          </Link>
-        ))}
+      <div ref={setRef} className="pb-3">
+        <h2 className="text-xl font-bold">
+          <FontAwesomeIcon icon={faEdit} className="mr-3" />
+          {t('edit-mode')}
+        </h2>
+        <div role="tablist" className="tabs tabs-border mt-3">
+          {EDIT_TABS.map(tab => (
+            <Link
+              role="tab"
+              className={classNames('tab !text-primary-content', {
+                'tab-active !text-accent-content': location.pathname === tab.id,
+              })}
+              to={tab.id}
+              key={tab.id}
+            >
+              {t(tab.label)}
+            </Link>
+          ))}
+        </div>
       </div>
-      <Outlet />
+      <div className="overflow-auto" style={outletWrapper}>
+        <Outlet />
+      </div>
     </div>
   );
 }
