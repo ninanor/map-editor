df8b242fcc60d1bda8ad4433ffd60c736c5b43c8
Niccolo Cantu <niccolo.cantu@nina.no>
2025-08-15 15:15:23 +0200

    fix: break long words

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -2,12 +2,13 @@ import Markdown from 'react-markdown';
 import { autoLinkMd } from 'react-markdown-autolink';
 
 import { useMemo } from 'react';
+import classNames from 'classnames';
 
 export function Description({ text, className = 'prose prose-slate prose-md' }: { text: string; className?: string }) {
   const linked = useMemo(() => autoLinkMd(text), [text]);
 
   return (
-    <div className={className}>
+    <div className={classNames(className, 'dont-break-out')}>
       <Markdown>{linked}</Markdown>
     </div>
   );
