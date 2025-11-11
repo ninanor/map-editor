87c1790cf8f0046144cd421dcd0668480f07475c
Niccolo Cantu <niccolo.cantu@nina.no>
2025-11-11 13:47:35 +0100

    chg: handle tab error

--- src/routes/$mapId/_layout/_view.tsx
+++ src/routes/$mapId/_layout/_view.tsx
@@ -1,9 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, ErrorComponent, ErrorComponentProps } from '@tanstack/react-router';
 import { TitleTabs } from '../../../components/TitleTabs';
 
 export const Route = createFileRoute('/$mapId/_layout/_view')({
   component: RouteComponent,
+  errorComponent: TabErrorComponent,
 });
+
+function TabErrorComponent({ error }: ErrorComponentProps) {
+  return (
+    <div>
+      <ErrorComponent error={error} />
+    </div>
+  );
+}
 
 function RouteComponent() {
   const params = Route.useParams();
