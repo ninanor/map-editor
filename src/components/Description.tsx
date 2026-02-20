d04f779aaa7dc50e38db9f891bb5fb7b466f3835
Niccolo Cantu <niccolo.cantu@nina.no>
2026-02-20 11:49:31 +0100

    fix: typescript issues

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -3,8 +3,8 @@ import { useMemo } from 'react';
 import Markdown from 'react-markdown';
 import { autoLinkMd } from 'react-markdown-autolink';
 
-export function Description({ text, className = 'prose prose-slate prose-md' }: { text: string; className?: string }) {
-  const linked = useMemo(() => autoLinkMd(text), [text]);
+export function Description({ text, className = 'prose prose-slate prose-md' }: { text?: string; className?: string }) {
+  const linked = useMemo(() => (text ? autoLinkMd(text) : ''), [text]);
 
   return (
     <div className={classNames(className, 'dont-break-out')}>
