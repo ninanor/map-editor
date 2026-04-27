d7223a150bfebf7994810800be802702e242c2c7
Niccolo Cantu <niccolo.cantu@nina.no>
2026-04-27 11:55:41 +0200

    fix: switch no postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { resolve } from 'node:path';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/postcss';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
@@ -7,10 +7,15 @@ import wasm from 'vite-plugin-wasm';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [wasm(), tanstackRouter({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+  plugins: [wasm(), tanstackRouter({ target: 'react', autoCodeSplitting: true }), react()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
     },
   },
   build: {
